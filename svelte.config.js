0a74628a09687bddd47a06325b9bb660176c24ac
Tina <202310991@gordoncollege.edu.ph>
2025-12-09 01:07:41 +0800

    to depply update?

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,11 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-    preprocess: vitePreprocess(),
-
+export default {
     kit: {
-        adapter: adapter(),
-        prerender: {
-            entries: ['/', '/catalog', '/library']
-        }
-    }
-};
-
-export default config;
+        adapter: adapter({
+            fallback: 'index.html'
+        })
+    },
+    preprocess: vitePreprocess()
+}; 
